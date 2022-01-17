924824ca9554585c4a65af5b45be401de4d6a685
shiba517 <shiba.cdeb@gmail.com>
2022-01-17 17:47:10 +0000

    form for adding products works based on the session['cart'] updating as intended

--- crate/urls.py
+++ crate/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.crate, name='crate'),
+    path('add/<item_id>/', views.add_to_crate, name='add_to_crate'),
 ]
--- crate/views.py
+++ crate/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
 
+
 def crate(request):
 
-    
-
     return render(request, 'crate/crate.html')
+
+
+def add_to_crate(request, item_id):
+    # item_id has been set to the value of product.id via <input> in products/templates/products/products.html
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    
+    crate = request.session.get('crate', {})
+
+    if item_id in list(crate.keys()):
+        crate[item_id] += quantity
+    else:
+        crate[item_id] = quantity
+
+    request.session['crate'] = crate
+    print(request.session['crate'])
+    return redirect(redirect_url)
 
 
