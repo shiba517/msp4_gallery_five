494fae1bc87729ca7633645396e7f184a513304e
shiba517 <shiba.cdeb@gmail.com>
2022-01-18 23:43:26 +0000

    trying to recover from git reset incident. going well for now

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    
+    path('', views.checkout, name='checkout'),  
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+
 from .forms import OrderForm
 
 
