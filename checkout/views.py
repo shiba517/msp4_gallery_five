9739e2277baf6af30d8a31ce7a6dbef2a9441cb3
shiba517 <shiba.cdeb@gmail.com>
2022-01-26 12:54:01 +0000

    could not checkout successfully whilst a non user. now can

--- checkout/views.py
+++ checkout/views.py
@@ -130,7 +130,6 @@ def checkout(request):
 
     return render(request, template, context)
 
-@login_required()
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
